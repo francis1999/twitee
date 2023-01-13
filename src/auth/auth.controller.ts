f8cfcf8511c5228d5f1c0a9653b8bf49d8bd0b58
zercominternalrepo <repo@zercomsystems.com>
2023-01-13 22:40:59 +0100

    first commit for twitee app

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller("auths")
+export class AuthController{
+    constructor(private authService: AuthService){}
+   
+    @Post("signup")
+    signup(){
+        return this.authService.signup()
+    }
+
+
+}
