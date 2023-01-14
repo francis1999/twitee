9c872a1a0207336a5fdee34a4d2aad4f0ba85270
zercominternalrepo <repo@zercomsystems.com>
2023-01-14 11:18:47 +0100

    authentication continues

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaService){}
-    signup(){
-        return this.prisma;
+    async signup(dto:AuthDto){
+        //generate the password hash
+        const hash=await argon.hash(dto.password)
+        const email = dto.email;
+        const username = email.substring(0,email.indexOf('@'))
+        //reurn the saved user
+        const auth = await this.prisma.auth.create({
+           
+                data:{
+                    email:dto.email,
+                    hash,
+                    name:dto.name,
+                    userName:username
+                }
+        })
+        return auth; 
     }
 }
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,31 @@
+
+//to apply validation, we will change the interface to class
+
+import { IsEmail, IsNotEmpty,IsString } from "class-validator"
+
+export class AuthDto{
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+    
+    @IsNotEmpty()
+    @IsString()
+    password: string
+
+    @IsString()
+    name:string
+
+    userName:string
+}
+
+export class LoginDto{
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+    
+    @IsNotEmpty()
+    @IsString()
+    password: string
+
+   
+}
